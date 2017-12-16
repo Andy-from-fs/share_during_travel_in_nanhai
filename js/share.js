eb56ad46662c169a66a2be0138fc91376ec746ae
Andy <735188273@qq.com>
2017-12-16 18:24:25 +0800

    map 定位 缩放

--- js/share.js
+++ js/share.js
@@ -80,3 +80,55 @@ $("#fileupload").fileupload({
     $.loading.off();
   }
 });
+
+(function() {
+  var map = new BMap.Map("map");
+  var point = new BMap.Point(116.331398, 39.897445);
+  map.centerAndZoom(point, 16);
+  var navigation = new BMap.NavigationControl({
+    anchor:BMAP_ANCHOR_BOTTOM_RIGHT,
+    // offset:new BMap.Size(0,0),
+    type:BMAP_NAVIGATION_CONTROL_LARGE,
+    showZoomInfo:false,
+    enableGeolocation:true
+  }); 
+  map.addControl(navigation);
+
+  var geolocation = new BMap.Geolocation();
+  geolocation.getCurrentPosition(
+    function(r) {
+      console.log(r);
+      if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+        var mk = new BMap.Marker(r.point);
+        map.addOverlay(mk);
+        map.panTo(r.point);
+        swal(
+          "定位成功",
+          '你的位置:'+r.address.province+" "+r.address.city,
+          'success'
+        )
+        
+        console.log("您的位置：" + r.point.lng + "," + r.point.lat);
+      } else {
+        swal(
+          "定位失败",
+          '请检查你的网路与允许网页获取你的位置信息',
+          'error'
+        )
+        console.log("failed" + this.getStatus());
+        //关于状态码
+        //BMAP_STATUS_SUCCESS	检索成功。对应数值“0”。
+        //BMAP_STATUS_CITY_LIST	城市列表。对应数值“1”。
+        //BMAP_STATUS_UNKNOWN_LOCATION	位置结果未知。对应数值“2”。
+        //BMAP_STATUS_UNKNOWN_ROUTE	导航结果未知。对应数值“3”。
+        //BMAP_STATUS_INVALID_KEY	非法密钥。对应数值“4”。
+        //BMAP_STATUS_INVALID_REQUEST	非法请求。对应数值“5”。
+        //BMAP_STATUS_PERMISSION_DENIED	没有权限。对应数值“6”。(自 1.1 新增)
+        //BMAP_STATUS_SERVICE_UNAVAILABLE	服务不可用。对应数值“7”。(自 1.1 新增)
+        //BMAP_STATUS_TIMEOUT	超时。对应数值“8”。(自 1.1 新增)
+      }
+    },
+    { enableHighAccuracy: true }
+  );
+})();
+
