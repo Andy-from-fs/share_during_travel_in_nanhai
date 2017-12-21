c45979172a1246410dde868e2da4d6a0794bb37d
Andy <735188273@qq.com>
2017-12-21 19:05:06 +0800

    改写增加遮罩层功能

--- js/loading.js
+++ js/loading.js
@@ -4,6 +4,7 @@
     this.init = function() {
       $("body").append(
         '<!-- loading start -->\
+        <div class="loading-wrap" id="loading-wrapper"></div>\
       <div class="loading" id="aLoading">\
         <div class="windows8">\
           <div class="wBall" id="wBall_1">\
@@ -30,23 +31,47 @@
       </div>\
       <!-- loading end -->'
       );
+      function preventVerticalDraft(node) {
+        $(node).on("touchstart", function() {
+          $(node).on("touchmove", function(e) {
+            var touch = e.originalEvent.targetTouches[0];
+            var y = touch.pageY;
+            var x = touch.pageX;
+            if (x > 0 || y > 0) {
+              e.stopPropagation();
+              e.preventDefault();
+            }
+          });
+        });
+        $(node)
+          .on("touchend", function() {
+            $(node).off("touchmove");
+          })
+          .on("click", function() {
+            e.stopPropagation();
+            e.preventDefault();
+          });
+        return $(node);
+      }
       this.ele = $("#aLoading");
+      this.wrapper = $("#loading-wrapper");
       this.word = $("#aLoadingWord");
+      preventVerticalDraft($(this.ele));
       this.init = function() {};
     };
   }
-  loading.prototype.turn = function(text) {
+  loading.prototype.turn = function(text,isWrapper) {
     if (this.isShow) {
       this.off();
     } else {
       if (text) {
-        this.on(text);
+        this.on(text,isWrapper);
       } else {
         this.on();
       }
     }
   };
-  loading.prototype.on = function(text) {
+  loading.prototype.on = function(text,isWrapper) {
     if (this.isShow) {
     } else {
       this.init();
@@ -55,15 +80,20 @@
       } else {
         $(this.word).html("加载中");
       }
+      console.log(isWrapper);
+      if(isWrapper){
+        $(this.wrapper).fadeIn();
+        $(this.ele).addClass('transparent-bg');
+      }
       $(this.ele).fadeIn();
       this.isShow = true;
     }
   };
   loading.prototype.off = function() {
     if (this.isShow) {
-      $(this.ele).fadeOut();
+      $(this.ele).fadeOut().removeClass('transparent-bg');
+      $(this.wrapper).fadeOut();
       this.isShow = false;
-    } else {
     }
   };
   $.loading = new loading();
